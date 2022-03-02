5240c145e8b644445d182b18153966a4e9b806e3
xiaochaddy <100772585+xiaochaddy@users.noreply.github.com>
2022-03-02 19:41:33 +0000

    Update Task_5_Chad_Hoang_stepdefs.js

--- test/stepDefs/Task_5_Chad_Hoang_stepdefs.js
+++ test/stepDefs/Task_5_Chad_Hoang_stepdefs.js
@@ -1,7 +1,9 @@
 const { Given } = require('@cucumber/cucumber')
 const Task_5_Chad_Hoang_Objects = require('../pageObjects/simpleSearchPageObject')
+const SimpleSearchPageObject = require('../pageObjects/simpleSearchPageObject')
 
-const Task_5_Chad_Hoang_Objects = new SimpleSearchPageObject()
+const Task_5_Chad_Hoang_Objects = new Task_5_Chad_Hoang_Object()
+const simpleSearchPageObject = new SimpleSearchPageObject()
 
 Given('I am on the studentbeans homepage', function () {
   simpleSearchPageObject.goToHomePage()
@@ -9,17 +11,17 @@ Given('I am on the studentbeans homepage', function () {
 })
 
 And('I open the login screen', function() {
-  Task_5_Chad_Hoang_Objects.goToLoginPage()
-  Task_5_Chad_Hoang_Objects.verifyLoginPage()
+  Task_5_Chad_Hoang_Object.goToLoginPage()
+  Task_5_Chad_Hoang_Object.verifyLoginPage()
 })
 
 When('I enter "test@test.com" in the email text box and "testingstuff" in the password text box', function () {
-  Task_5_Chad_Hoang_Objects.typeEmail()
-  Task_5_Chad_Hoang_Objects.typePassword()
-  Task_5_Chad_Hoang_Objects.clickCaptcha()
-  Task_5_Chad_Hoang_Objects.clickLogin()
+  Task_5_Chad_Hoang_Object.typeEmail()
+  Task_5_Chad_Hoang_Object.typePassword()
+  Task_5_Chad_Hoang_Object.clickCaptcha()
+  Task_5_Chad_Hoang_Object.clickLogin()
 })
 
 Then('I should see the error message display on screen', function () {
-  Task_5_Chad_Hoang_Objects.verifyErrorMessage()
+  Task_5_Chad_Hoang_Object.verifyErrorMessage()
 })
